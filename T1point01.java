aa33a7ae525a8b6949b9e1b3a567215caa699e17
gusangga <101165406+gusangga@users.noreply.github.com>
2022-03-31 02:49:17 -0700

    Setor Tugas 1

---
+++ T1point01.java
@@ -0,0 +1,15 @@
+package t1point01;
+
+public class T1point01 {
+
+    public static void main(String[] args) {
+        String[] nama    = {"Adi", "Mika", "Budi", "Bunga", "Bayu"};
+        
+        System.out.println(nama[0]);
+        System.out.println(nama[1]);
+        System.out.println(nama[2]);
+        System.out.println(nama[3]);
+        System.out.println(nama[4]);
+    }
+    
+}
---
+++ T2point02.java
@@ -0,0 +1,16 @@
+package t2point02;
+
+public class T2point02 {
+
+    public static void main(String[] args) {
+        String[] nama    = {"Adi", "Mika", "Budi", "Bunga"}; 
+        int[] nim        = {127647, 129883, 120495, 123489};
+        String[] jurusan = {"Teknik Informatika", "Sistem Komputer", "Sistem Komputer", "Teknik Informatika"};
+        
+        System.out.println("Data siswa 1 : "+(nama[0])+" "+nim[0]+" "+jurusan[0]);
+        System.out.println("Data siswa 2 : "+(nama[1])+" "+nim[1]+" "+jurusan[1]);
+        System.out.println("Data siswa 3 : "+(nama[2])+" "+nim[2]+" "+jurusan[2]);
+        System.out.println("Data siswa 4 : "+(nama[3])+" "+nim[3]+" "+jurusan[3]);
+    }
+    
+}
